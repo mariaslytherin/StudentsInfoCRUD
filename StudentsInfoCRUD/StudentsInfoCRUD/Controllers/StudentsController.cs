04e4d655825c55bcc7a216200862e9e05f088971
DESKTOP-AUBDH4M\Maria <maria.pirinova39@gmail.com>
2024-03-23 18:41:53 +0200

    Add simple crud operations for students

---
+++ StudentsInfoCRUD/StudentsInfoCRUD/Controllers/StudentsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentsInfoCRUD.Data;
+using StudentsInfoCRUD.Data.Entities;
+
+namespace StudentsInfoCRUD.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private AppDbContext _appDbContext;
+
+        public StudentsController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Student student)
+        {
+           _appDbContext.Students.Add(student);
+            _appDbContext.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            var students = _appDbContext.Students.ToList();
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var student = _appDbContext.Students.Find(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Student student)
+        {
+            var oldStudent = _appDbContext.Students.Find(student.Id);
+            oldStudent.FirstName = student.FirstName;
+            oldStudent.LastName = student.LastName;
+            oldStudent.FacultyNumber = student.FacultyNumber;
+            oldStudent.Faculty = student.Faculty;
+            _appDbContext.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var student = _appDbContext.Students.Find(id);
+            _appDbContext.Students.Remove(student);
+            _appDbContext.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
--- StudentsInfoCRUD/StudentsInfoCRUD/Program.cs
+++ StudentsInfoCRUD/StudentsInfoCRUD/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Students}/{action=List}/{id?}");
 
 app.Run();
